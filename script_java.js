b4312dd6da179aef04761b277d07eb1e67eebf4e
Bruno Borges <bruninho-borgesf@hotmail.com>
2021-05-31 17:37:44 -0300

    Atualização do Front-End para V3.0 - Beta

---
+++ script_java.js
@@ -0,0 +1,36 @@
+
+function Padrao(){
+    window.location.assign("compras/padrao.html");
+}
+function Pro(){
+    window.location.assign("compras/pro.html");
+}
+function Ultra(){
+    window.location.assign("compras/ultra.html");
+}
+function credito(){
+    alert("Em breve - 'Crédito' estará disponível!");
+}
+function debito(){
+    alert("Em breve - 'Débito' estará disponível!");
+}
+function boleto(){
+    alert("Em breve - 'Boleto' estará disponível!");
+}
+function cadastroUsuario(){
+    window.location.assign("cadastroUsuario.php");
+}
+function verUsuarios(){
+    window.location.assign("verUsuarios.php");
+}
+function excliurUsuario(){
+    alert("Em breve - 'Excluir Usuário' estará disponível!");
+}
+function vendasEfetudas(){
+    alert("Em breve - 'Vendas Efetudas' estará disponível!");
+}
+function produtos(){
+    alert("Em breve - 'Produtos' estará disponível!");
+}
+
+//<script src="C:\Users\Guilherme\Desktop\Técnico T.I\Projeto Integrador\Projeto Senac\script_java.js"></script>
